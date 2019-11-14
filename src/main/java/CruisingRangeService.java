16f1d2a5606584f5f8c00115c3cd9085039e860d
student <student@localhost>
2019-11-14 15:10:30 +0300

    fixes #1

--- src/main/java/CruisingRangeService.java
+++ src/main/java/CruisingRangeService.java
@@ -2,10 +2,10 @@ public class CruisingRangeService {
     public int cruisingRange(int remainingFuelInMilliliters, int fuelConsumptionPer100KmInMilliliters) {
         if (fuelConsumptionPer100KmInMilliliters <= 0) {
             return 0;
-        } else if (remainingFuelInMilliliters < 0) {
-            return 0;
-        } else {
-            return remainingFuelInMilliliters * 100 / fuelConsumptionPer100KmInMilliliters;
         }
+        if (remainingFuelInMilliliters < 0) {
+            return 0;
+        }
+            return remainingFuelInMilliliters * 100 / fuelConsumptionPer100KmInMilliliters;
     }
 }
